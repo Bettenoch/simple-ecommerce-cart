fcfb3a65185cdb200472d593582f440606afa57c
Bettenoch <bettenoch254@gmail.com>
2025-01-31 11:26:56 +0300

    Initialize vite project.

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='bg-amber-300 flex flex-col'>
+        <h1 className='text-7xl font-extrabold text-gray-950'>CHOOSE YOUR PRODUCTS TODAY</h1>
+
+      </div>
+    </>
+  )
+}
+
+export default App
