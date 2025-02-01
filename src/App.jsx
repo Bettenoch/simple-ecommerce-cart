cbc45bf5b402c96657b4474401925e3a0d94fc66
Bettenoch <bettenoch254@gmail.com>
2025-02-01 14:41:08 +0300

    Add route

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,20 @@
-import './App.css'
-import Products from './components/products/Products'
-import Cart from './components/Cart'
+// App.js
+
+import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Cart from './components/practice/Cart';
+import Home from './components/practice/Home';
+
 
 function App() {
-
-
-  return (
-    <>
-      <section className='w-full flex flex-col min-h-screen'>
-       <div className='bg-amber-300 flex flex-col'>
-       <h1 className='text-7xl font-extrabold text-gray-950 mb-8'>CHOOSE YOUR PRODUCTS TODAY</h1>
-       </div>
-       <div className='flex w-full flex-col'>
-       <Products/>
-       <Cart/>
-       </div>
-       
-
-      </section>
-    </>
-  )
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </div>
+    );
 }
 
-export default App
+export default App;
