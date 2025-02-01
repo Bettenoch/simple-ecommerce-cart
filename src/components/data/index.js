0a05a6eccbda8fb2ec2481d52a20a049da3bbedf
Bettenoch <bettenoch254@gmail.com>
2025-02-01 11:00:06 +0300

    Create data store

---
+++ src/components/data/index.js
@@ -0,0 +1,50 @@
+export const ProductList = [
+    {
+        id:1,
+        name: "shoe1",
+        price: 3900,
+        image: "./shoe1.jpg"
+    },
+    {
+        id:2,
+        name: "shoe2",
+        price: 3990,
+        image: "./shoe2.jpg"
+    },
+    {
+        id:3,
+        name: "shoe3",
+        price: 3490,
+        image: "./shoe1.jpg"
+    },
+    {
+        id:4,
+        name: "shoe4",
+        price: 3900,
+        image: "./shoe4.jpg"
+    }, 
+    {
+        id:5,
+        name: "shoe5",
+        price: 3900,
+        image: "./shoe1.jpg"
+    },
+    {
+        id:6,
+        name: "shoe6",
+        price: 3900,
+        image: "./shoe3.jpg"
+    },
+    {
+        id:7,
+        name: "shoe7",
+        price: 3900,
+        image: "./shoe1.jpg"
+    },
+    {
+        id:8,
+        name: "shoe8",
+        price: 24990,
+        image: "./shoe2.jpg"
+    },
+]
