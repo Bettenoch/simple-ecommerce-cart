9022668259e03b0881bd63c2bdd00b068d59876e
Bettenoch <bettenoch254@gmail.com>
2025-02-01 10:59:53 +0300

    Add cart reducer

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import cartReducer from './cartSlice'
 // first create a redux store
-export default Store = configureStore(
+export const store = configureStore(
     {
         reducer: {
+            cart: cartReducer,
 
         }
     }
 )
+export default store;
