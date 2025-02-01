e2c5f31b499e3d0fb390c869203fd6b531c78465
Bettenoch <bettenoch254@gmail.com>
2025-02-01 11:00:50 +0300

    Add elements to page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Products from './components/products/Products'
+import Cart from './components/Cart'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div className='bg-amber-300 flex flex-col'>
-        <h1 className='text-7xl font-extrabold text-gray-950'>CHOOSE YOUR PRODUCTS TODAY</h1>
+      <section className='w-full flex flex-col min-h-screen'>
+       <div className='bg-amber-300 flex flex-col'>
+       <h1 className='text-7xl font-extrabold text-gray-950 mb-8'>CHOOSE YOUR PRODUCTS TODAY</h1>
+       </div>
+       <div className='flex w-full flex-col'>
+       <Products/>
+       <Cart/>
+       </div>
+       
 
-      </div>
+      </section>
     </>
   )
 }
