8ffee3963d1d3cb4cfa51699b3f4ce9091314551
Bettenoch <bettenoch254@gmail.com>
2025-02-02 23:30:59 +0300

    Fix code

--- src/components/data/index.js
+++ src/components/data/index.js
@@ -3,48 +3,56 @@ export const ProductList = [
         id:1,
         name: "shoe1",
         price: 3900,
-        image: "./shoe1.jpg"
+        image: "./shoe1.jpg",
+        description: "Amazing Product"
     },
     {
         id:2,
         name: "shoe2",
         price: 3990,
-        image: "./shoe2.jpg"
+        image: "./shoe2.jpg",
+        description: "Amazing Product"
     },
     {
         id:3,
         name: "shoe3",
         price: 3490,
-        image: "./shoe1.jpg"
+        image: "./shoe1.jpg",
+        description: "Amazing Product"
     },
     {
         id:4,
         name: "shoe4",
-        price: 3900,
-        image: "./shoe4.jpg"
+        price: 3940,
+        image: "./shoe4.jpg",
+        description: "Amazing Product"
     }, 
     {
         id:5,
         name: "shoe5",
         price: 3900,
-        image: "./shoe1.jpg"
+        image: "./shoe1.jpg",
+        description: "Amazing Product"
     },
     {
         id:6,
         name: "shoe6",
         price: 3900,
-        image: "./shoe3.jpg"
+        image: "./shoe3.jpg",
+        description: "Amazing Product"
     },
     {
         id:7,
         name: "shoe7",
         price: 3900,
-        image: "./shoe1.jpg"
+        image: "./shoe1.jpg",
+        description: "Amazing Product"
     },
     {
         id:8,
         name: "shoe8",
         price: 24990,
-        image: "./shoe2.jpg"
+        image: "./shoe2.jpg",
+        description: "Amazing Product"
     },
 ]
