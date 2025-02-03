3a0f5107ea2445b868ebf604584b01d9fefac524
Bettenoch <bettenoch254@gmail.com>
2025-02-03 13:33:52 +0300

    Add search reducer

--- src/components/redux/store.jsx
+++ src/components/redux/store.jsx
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from './cartSlice'
+import searchReducer from './searchSlice';
 // first create a redux store
 export const store = configureStore(
     {
         reducer: {
             cart: cartReducer,
+            search: searchReducer,
 
         }
     }
