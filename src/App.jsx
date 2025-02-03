924c05116d9a126bec6e95c6740b359950b3b4a0
Bettenoch <bettenoch254@gmail.com>
2025-02-03 13:34:01 +0300

    Rename products

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,15 @@
 import './App.css';
 import Cart from './components/assign/Cart';
 import CartDisplay from './components/assign/CartDisplay';
-import Products from './components/assign/Products';
-
-
+import Products2 from './components/assign/Products2';
+//import Products from './components/assign/Products';
 
 
 function App() {
     return (
         <div className="App">
             <CartDisplay/>
-            <Products/>
+            <Products2/>
             <Cart/>
         </div>
     );
